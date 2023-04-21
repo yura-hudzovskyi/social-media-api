feafccdac4b9f40f2f3b52a376a14321ea180f20
Yura Hudzovskiy <50211819+yura-hudzovskyi@users.noreply.github.com>
2023-04-21 12:30:42 +0300

    Add User model with image field & email and password authentication

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "api",
+    "User",
 ]
 
 MIDDLEWARE = [
@@ -113,11 +114,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = "User.User"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
